f1f4324bebd4bee7412e15d6ada71f6053cf123a
Dominique8D <dejong.dominique@gmail.com>
2025-05-13 11:40:14 +0000

    Try fix pages

--- project-vue/vitest.config.ts
+++ project-vue/vitest.config.ts
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: '/project-vue/',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
